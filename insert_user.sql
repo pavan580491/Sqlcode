463cac63c6c73d0b3d660f574f8d78128789c996
pavan580491 <pavan.karbonn@gmail.com>
2024-12-11 15:41:53 +0530

    Create insert_user.sql

---
+++ insert_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (id,name,emai,password) VALUES (1,Pk,pk@gmai.com,1234);
+INSERT INTO user (id,name,emai,password) VALUES (2,aa,aa@gmai.com,1234);
+INSERT INTO user (id,name,emai,password) VALUES (3,nbk,nbk@gmai.com,1234);
+INSERT INTO user (id,name,emai,password) VALUES (4,mb,mb@gmai.com,1234);
